bed91281c30ae3c55332c28097c12c03f3cc2d12
JesusGC290 <jesus@epik.ai>
2026-05-30 23:15:48 -0600

    First site

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        base: '#1C1917',
+        secondary: '#F5F5F4',
+        accent: '#78866B',
+      },
+      fontFamily: {
+        outfit: [
+          'Outfit',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+        ],
+        inter: [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+        ],
+      },
+    },
+  },
+};
